81d7cd983166a8b444aff1a4346d877490ce94e1
Davide <dav.valiante@gmail.com>
2018-08-28 23:18:34 +0200

    fixati import sbagliati dopo il merge con Zuldru

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,17 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Responsive, Segment, Sticky, Grid, Header, Container, Icon } from "semantic-ui-react";
 import SpikeHeader from "./Header";
-import BonusList from "./HomeComponents/BonusList";
 import Footer from "./Footer";
-import { Button } from "semantic-ui-react/dist/commonjs/elements/Button/Button";
-import { setUserPlaying } from "../reducers/PlayModeReducer";
-import SlotPageBonusList from "./SlotPageComponents/SlotPageBonusList";
 import { Link } from 'react-router-dom';
 import { Image } from 'semantic-ui-react'
 import { connect } from 'react-redux'
-import { ROUTE } from './enums/Constants'
-import { setAboutPage } from './reducers/CurrentPageReducer'
-import { smoothScrollTo } from './utils/Utils'
 
 const AboutPage = (props) => {
 
@@ -49,14 +42,14 @@ const AboutPage = (props) => {
                                 iniziano ad essere seguiti. Solo nel 2014 inizio a dedicarmici seriamente, e
                                 nasce la mitica serie <strong>&#8220;Giorni di Ordinaria
                                     Follia&#8221;</strong> in cui vi mostro una giornata tipo: insieme ai miei
-                Soci andiamo in giro a giocare alle Slot, con varie gag divertenti tra una
+            Soci andiamo in giro a giocare alle Slot, con varie gag divertenti tra una
                                 partita e l&#8217;altra.</p>
                             <p>Poi per la passione che da sempre ho per l&#8217;informatica e il mondo
                                 digitale, <strong>SPIKE</strong> si è trasformato in qualcosa di più.</p>
                             <p>Appoggiandosi a <Link to="/"> spikeslot </Link>  e al
                                 forum <a href="http://www.Professioneslot.it"
                                     target="_blank"> Professioneslot </a>, è diventato il principale
-                (e forse unico) strumento informativo e di dialogo veramente competente in
+            (e forse unico) strumento informativo e di dialogo veramente competente in
                                 materia, <strong>per tutti i giocatori di Slot Machine d&#8217;Italia.</strong>
                             </p>
                             <p>&nbsp;</p>
