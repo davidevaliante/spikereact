fa18f570df3ecd7aa4560b6badde7d48d29c4be5
Davide <dav.valiante@gmail.com>
2018-08-29 16:14:16 +0200

    fixata deprecazione in AdminDashboard

--- src/admin/AdminDashboard.js
+++ src/admin/AdminDashboard.js
@@ -1,10 +1,10 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component, ReactDOM } from 'react';
+import { connect } from 'react-redux';
 import AddSlot from "./AdminSlots/AddSlot";
 import AdminNavbar from "./AdminNavbar";
 import AddArticle from "./AddArticle";
-import {ADMINPAGES} from "../enums/Constants";
-import {Responsive} from "semantic-ui-react";
+import { ADMINPAGES } from "../enums/Constants";
+import { Responsive } from "semantic-ui-react";
 import Header from "../components/Header";
 import SlotDashboard from "./AdminSlots/SlotDashboard";
 
@@ -13,15 +13,15 @@ class AdminDashboard extends Component {
     state = {};
 
     handleClick = (childComponent) => {
-        console.log("childComponent:",childComponent)
-        console.log(React.findDOMNode(this.refs.theChild))
+        console.log("childComponent:", childComponent)
+        console.log(ReactDOM.findDOMNode(this.refs.theChild))
     };
 
     render() {
         console.log(this.props.match.path);
         return (
             <Responsive>
-                <AdminNavbar activeItem={ADMINPAGES.ADMIN}/>
+                <AdminNavbar activeItem={ADMINPAGES.ADMIN} />
                 "Im Admin DashBoard component"
             </Responsive>
         );
@@ -32,4 +32,4 @@ function mapStateToProps(state) {
     return {};
 }
 
-export default connect(mapStateToProps,)(AdminDashboard);
+export default connect(mapStateToProps)(AdminDashboard);
