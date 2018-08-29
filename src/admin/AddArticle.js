428681a031a20243bfa1d8ef22eeee5d9375ecd8
Davide <dav.valiante@gmail.com>
2018-08-29 16:05:07 +0200

    finite funzionalità di base del component stile 'Medium' può già essere utilizzato per tips e tecnicals

--- src/admin/AddArticle.js
+++ src/admin/AddArticle.js
@@ -3,92 +3,240 @@ import AddRow from './AddRow'
 import { Divider, Icon, Dropdown, Input } from 'semantic-ui-react'
 import _ from 'lodash'
 
-class AddArticle extends Component {
 
-    TextComponentRenderer = ({ position, content }) => {
-        return (
-            <div className='row-container'>
-                <h2
-                    style={{ width: '100%' }}
-                    fluid
-                    placeholder={`Inserisci riga numero ${position}`}>{content}</h2>
-                <Icon color='red' name='delete' onClick={(event) => this.handleDelete(position)} />
-            </div>
-        );
-    }
+// -------PROPS---------------------
+// onItemAdded -> callback
+// rowRenderer
+class AddArticle extends Component {
 
     state = {
         rows: [
 
-        ]
+        ],
+        inputPosition: 0
     }
 
-    handleDelete = positionToRemove => {
+    /* 
+        RICEVE ->
+                position : la posizione in {this.state.rows} dell'elemento da renderizzare
+                content  : contenuto (testo) da renderizzare
+        DESCRIZIONE ->   
+                restituisce un component di testo per ogni elemento in state.rows
+                quando viene cliccato dve permettere di editare il contenuto mantenendo 
+                la giusta posizione nell'array e spostando l'input nella posizione giusta
+        FUNZIONI CORRELATE ->
+                editItemOnPosition()
+                handleDelete()
+        NOTE ->
+                la notazione { position, content } estrae direttamente i valori dai props
+                che hanno lo stesso nome
+    */
+    TextComponentRenderer = ({ position, content }) => {
+        return (
+            <div style={{ width: '100%' }}>
+                <div className='row-container'>
+                    <h2
+                        style={{ width: '100%' }}
+                        fluid
+                        action='Elimina'
+                        onClick={() => this.editItemOnPosition(position, { content })}>
+                        {content}
+                    </h2>
+                    <Icon color='red' name='delete' onClick={(event) => this.handleDelete(position)} />
+                </div>
+            </div>
+        );
+    }
+
+    /*
+        RICEVE ->
+                position     : la posizione dell'elemento nell'array da modificare
+                currentValue : il valore attuale che si sta modificando
+        DESCRIZIONE ->
+                facendo l'update dello state tutto il componente viene renderizzato di nuovo.
+                tramite {position} stabiliamo dove far comparire l'input nella lista e tramite
+                currentValue settiamo l'input di default con il valore da modificare
+        FUNZIONI CORRELATE ->
+                render()
+    */
+    editItemOnPosition = (position, currentValue) => {
+        this.setState({ inputPosition: position, editValue: currentValue })
+    }
+
+    /*
+        RICEVE ->
+                positionToRemove : la posizione dell'elemento nell'array da rimuovere
+        DESCRIZIONE ->
+                rimuove attraverso _.filter() l'elemento da eliminare e aggiorna lo state.
+                di base setta {inputPosition} pari alla lunghezza della lista (già filtrata)
+                questo fa comparire l'input come ultima riga dopo ogni "delete" (vedi rowsRenderer)
+        FUNZIONI CORRELATE ->
+                rowsRenderer()
+                render()
+    */
+    handleDelete = (positionToRemove) => {
         console.log(`removing ${positionToRemove}`);
-
-        this.setState(prevState => ({ rows: _.filter(prevState.rows, (value, index, collection) => index !== positionToRemove) }))
-    }
-
-    setContentToPosition = (toPosition, newContent) => {
-        this.setState(prevState => (
-            {
-                rows: prevState.rows.map((row, position) => {
-                    if (position === toPosition) {
-                        row['content'] = newContent
-                        return row
-                    } else {
-                        return row
-                    }
-                })
+        this.setState(prevState => {
+            const updatedRowList = _.filter(prevState.rows, (value, index, collection) => index !== positionToRemove)
+            return {
+                rows: updatedRowList,
+                inputPosition: updatedRowList.length
             }
-        )
+        }
         )
     }
 
-    onKeyDown = (event) => {
+    /* 
+        RICEVE ->
+                defaultValue : il valore di default dell' Input. E' presente solo se si sta
+                               editando una riga, altrimenti comapare il placeholder 
+        DESCRIZIONE ->   
+                restituisce un component di Input che ha un valore di default SOLO se si sta
+                editando una riga. 
+        FUNZIONI CORRELATE ->
+                onKeyDown()
+    */
+    InputComponentRenderer = ({ position, defaultValue }) => {
+        return (
+            <div style={{ width: '100%', display: 'flex', flexDirection: 'column' }}>
+                <div style={{ padding: '.5rem' }}>
+                    <Input
+                        id='input'
+                        autoFocus
+                        onKeyDown={(event) => this.onKeyDown(event, position)}
+                        style={{ width: '100%' }}
+                        fluid
+                        transparent
+                        defaultValue={defaultValue}
+                        placeholder='Inserisci' />
+                </div>
+                <div style={{ background: 'red', width: '100%', height: '1px' }}></div>
+            </div>
+        )
+    }
+
+    /* 
+        RICEVE ->
+                event   : passato da Semantic (che a sua volta passa quello di base di React)
+                currentInputPosition : serve a stabilire contemporaneamente se e cosa si sta editando
+        DESCRIZIONE ->   
+               aggiorna lo state. Aggiunge una nuova riga alla fine dell'array o aggiorna una riga esistente
+        FUNZIONI CORRELATE ->
+                _.map()
+                render()
+    */
+    onKeyDown = (event, currentInputPosition) => {
+
+        // solo per invio, altrimenti intercetta tutti i tasti e solo con contenuto valido
         if (event.key === 'Enter' && document.getElementById('input').value.trim()) {
+
+            console.log(currentInputPosition);
+
+            // rappresentazione in JSON di una nuova riga
             const objectToAdd = {
                 type: 'text',
                 editable: true,
                 content: document.getElementById('input').value.trim()
             }
-            this.setState(prevState => ({ rows: [...prevState.rows, objectToAdd] }))
-            document.getElementById('input').value = ''
+
+            // se la posizione attuale dell'input è l'ultima si sta aggiungendo una nuova riga
+            if (currentInputPosition === this.state.rows.length) {
+                // la nuova riga va aggiunta alla fine dell'array {rows: [...prevState.rows, objectToAdd]}
+                this.setState(prevState => (
+                    {
+                        rows: [...prevState.rows, objectToAdd],
+                        inputPosition: prevState.inputPosition + 1,
+                        editValue: undefined
+                    }))
+                // resetta il valore dell'input
+                document.getElementById('input').value = ''
+            }
+            // altrimenti vuol dire che si sta editando e il nuovo oggetto va inserito nella posizione giusta
+            else {
+                this.setState(prevState => (
+                    {
+                        rows: prevState.rows.map((row, index) => index === currentInputPosition ? objectToAdd : row),
+                        inputPosition: prevState.rows.length,
+                        editValue: undefined
+                    }))
+            }
+
         }
     }
 
 
-    rowsRenderer = () => {
-        return this.state.rows.map((row, position) => {
-            switch (row.type) {
-                case 'text':
-                    return <this.TextComponentRenderer position={position} content={this.state.rows[position].content} />
-
-                default:
-                    return <Input
-                        id='input'
-                        onKeyDown={this.onKeyDown}
-                        style={{ width: '100%' }}
-                        fluid
-                        value={this.state.rows[position].content}
-                        placeholder='Inserisci' />
+    /* 
+        RICEVE ->
+                inputPosition   : in che riga far comparire l'Input
+                editValue       : se presete viene passata all'input come default value
+        DESCRIZIONE ->   
+               restituisce una lista di righe, cosa renderizzare va in base a state.rows[index].type
+        FUNZIONI CORRELATE ->
+                _.map()
+        NOTE -> 
+                ampliabile facilmente per mostrare qualunque tipo di contenuto
+    */
+    rowsRenderer = (inputPosition, editValue) => {
+        // cicla l'array di righe presenti nello state
+        const formattedRows = this.state.rows.map((row, index) => {
+            // se l'elemento corrente ha un {index} pari alla posizione dell'input {inputPosition} che ci dice lo state
+            // allora in quella posizione dell'array dobbiamo mettere l'input
+            if (index === inputPosition) {
+                // editValue.content contiene il defaultValue (se presente)
+                return <this.InputComponentRenderer position={index} defaultValue={editValue.content} />
             }
+            // in tutti gli altri casi è una riga già confermata e si renderizza il component in base al suo {type}
+            else {
+                switch (row.type) {
+                    case 'text':
+                        return <this.TextComponentRenderer position={index} content={this.state.rows[index].content} />
+                    // ESEMPI DI MIGLIORAMENTO ->
+                    /*
+                    case 'image':
+                        return <ImageRendererComponent />
+                    case 'youtube':
+                        return <YoutubeEmbedRendererComponent />
+                    case 'link':
+                        return <LinkComponent />
+                    */
+                    default:
+                    // pass
+                }
+            }
+
         })
+
+        /* 
+            IMPORTANTE :
+            Se la funzione finisse qui dopo la prima riga scritta l'input spariirebbe, quindi
+            prendiamo {formattedRow} e gli pushiamo un InputComponent come ultimo elemento :
+
+            -> formattedRows.push(<this.InputComponentRenderer position={inputPosition} />)
+
+            facendo così però verrebbe aggiunto di base anche al primo render, quindi mettiamo la condizione:
+
+            -> if(inputPosition !== 0) formattedRows.push(<this.InputComponentRenderer position={inputPosition} />)
+
+            facendo così però verrebbe aggiunto un Input anche quando si sta editando un qualunque elemento tranne il primo
+            mentre vogliamo che sia presente solo un input alla volta, quindi
+            -> 
+             if (inputPosition !== 0 && inputPosition === formattedRows.length) formattedRows.push(<this.InputComponentRenderer position={inputPosition} />)
+
+        */
+        if (inputPosition !== 0 && inputPosition === formattedRows.length)
+            formattedRows.push(<this.InputComponentRenderer position={inputPosition} />)
+
+        return formattedRows
     }
 
     render() {
         console.log(this.state);
-
-
+        const { inputPosition, editValue } = this.state
+        if (editValue) document.getElementById('input').value = editValue
         return (
             <div className='main-column'>
-                {this.rowsRenderer()}
-                <Input
-                    id='input'
-                    onKeyDown={this.onKeyDown}
-                    style={{ width: '100%' }}
-                    fluid
-                    placeholder='Inserisci' />
+                {this.rowsRenderer(inputPosition, editValue)}
+                {(inputPosition === 0 && editValue === undefined) && <this.InputComponentRenderer position={0} />}
             </div>
         )
     }
