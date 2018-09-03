d22b2089e6bc79fe62b6e1a004be53eb92d68abd
Davide <dav.valiante@gmail.com>
2018-09-03 13:21:39 +0200

    Rimossa l'API di firebase database con axios, è rimasto solo lo storage (che andrà levato visto che serve solo nell'admin : peso totale aggiornato del bundle : 182.2 kb di JS e 102 di css

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase/app';
-import 'firebase/database'
+// import 'firebase/database'
 import 'firebase/storage'
 import { setUserLoggedIn, setUserLoggedOut, setUserName, setUserId } from './../reducers/AuthReducer';
 import { configuration } from './firebaseConfig';
@@ -10,9 +10,10 @@ import axios from 'axios'
 const config = configuration;
 const firebaseApp = firebase.initializeApp(config);
 
-export const firebaseDatabase = firebase.database();
-
-export const fromOnlineToGratis = () => {
+// export const firebaseDatabase = firebase.database();
+const databaseRoot = 'https://spike-2481d.firebaseio.com';
+// per trasformare da online a gratis
+/* export const fromOnlineToGratis = () => {
     axios.get('https://spike-2481d.firebaseio.com/Slots.json')
         .then(
             // success
@@ -34,9 +35,10 @@ export const fromOnlineToGratis = () => {
             // sempre eseguita
             () => { }
         )
-}
+} */
 
-export const onlineToGratis = (id, obj) => {
+// ------------------------------------------NON CANCELLARE PER ADESSO
+/* export const onlineToGratis = (id, obj) => {
     axios.patch(`https://spike-2481d.firebaseio.com/Slots/${id}/.json`, obj)
         .then(
             (success) => console.log('PATCH_SUCCESS', success)
@@ -44,9 +46,9 @@ export const onlineToGratis = (id, obj) => {
         .catch(
             (error) => console.log('PATCH_ERROR', error)
         )
-}
+} */
 
-export const pushNewSlotToRest = (id, obj) => {
+/* export const pushNewSlotToRest = (id, obj) => {
     axios.put(`https://spike-2481d.firebaseio.com/TestSlot/${id}/.json`, obj)
         .then(
             // fa automaticamente PUSH, l'id del push è contenuto in success.data.name
@@ -55,7 +57,7 @@ export const pushNewSlotToRest = (id, obj) => {
         .catch(
             (error) => console.log('POST_ERROR', error)
         )
-}
+} */
 
 
 export const pushNewImage = (image, folderName, callback) => {
@@ -75,8 +77,8 @@ export const pushNewImage = (image, folderName, callback) => {
         )
 }
 
-
-export const pushNewSlot = (newSlot, onPushSlotSuccess) => {
+// --------------------------------FIREBASE----------NON CANCELLARE PER ORA
+/* export const pushNewSlot = (newSlot, onPushSlotSuccess) => {
     pushNewImage(newSlot.image, STORAGE_FOLDERS.SLOT_IMAGES, (url) => {
         newSlot['image'] = url
         newSlot['time'] = now();
@@ -98,15 +100,41 @@ export const pushNewSlot = (newSlot, onPushSlotSuccess) => {
             )
     })
 
+} */
+
+export const pushNewSlot = (newSlot, onPushSlotSuccess) => {
+
+    pushNewImage(newSlot.image, STORAGE_FOLDERS.SLOT_IMAGES, (url) => {
+        newSlot['image'] = url
+        newSlot['time'] = now()
+        axios.post(`${databaseRoot}/Slots.json`, newSlot)
+            .then(
+                (success) => {
+                    const key = success.data.name;
+                    axios.put(`${databaseRoot}/Producer-Slot/${newSlot.producer.id}/${key}.json`, true)
+                    for (const bonusId in newSlot.bonus) {
+                        axios.put(`${databaseRoot}/Bonus-Slot/${bonusId}/${key}.json`, true)
+                    }
+                    onPushSlotSuccess();
+                }
+            )
+            .catch(
+                (error) => console.log(error)
+            )
+            .then(
+
+            )
+    })
+
 }
 
 
 export const pushNewProducer = (newProducer, callback) => {
     pushNewImage(newProducer.image, STORAGE_FOLDERS.PRODUCER_IMAGES, (url) => {
         newProducer['image'] = url
-        firebase.database().ref(DATABASE_REFERENCE.PRODUCER).push(newProducer)
+        axios.post(`${databaseRoot}/Producer.json`, newProducer)
             .then(
-                (snapshot) => {
+                (success) => {
                     callback()
                 }
             )
@@ -117,69 +145,110 @@ export const pushNewProducer = (newProducer, callback) => {
 export const pushNewBonus = (newBonus, onBonusPushSuccess) => {
     pushNewImage(newBonus.image, STORAGE_FOLDERS.BONUS_IMAGES, (url) => {
         newBonus['image'] = url
-        firebase.database().ref(DATABASE_REFERENCE.BONUS).push(newBonus)
+        axios.post(`${databaseRoot}/Bonus.json`, newBonus)
             .then(
-                ((snapshot) => {
-                    onBonusPushSuccess();
-                })
-            )
-            .catch(
-                (fail) => {
-                    console.log('New bonus push failed');
-                    console.log(fail);
+                (success) => {
+                    onBonusPushSuccess()
                 }
             )
     })
 }
 
-export const getBonusList = (callback) => {
+
+// ----------------------FIREBASE NON CANCELLARE
+/* export const getBonusList = (callback) => {
     firebaseDatabase.ref(DATABASE_REFERENCE.BONUS).once('value')
         .then(
             (snapshot) => {
                 callback(snapshot.val());
             }
         )
+} */
+
+export const getBonusList = (callback) => {
+    axios.get(`${databaseRoot}/Bonus.json`)
+        .then(
+            success => callback(success.data)
+        )
 }
+
+// ----------------------FIREBASE NON CANCELLARE
+
+// export const getProducerList = (callback) => {
+//     firebaseDatabase.ref(DATABASE_REFERENCE.PRODUCER).once('value')
+//         .then(
+//             (snapshot) => callback(snapshot.val())
+//         )
+// }
 
 export const getProducerList = (callback) => {
-    firebaseDatabase.ref(DATABASE_REFERENCE.PRODUCER).once('value')
+    axios.get(`${databaseRoot}/Producer.json`)
         .then(
-            (snapshot) => callback(snapshot.val())
+            success => callback(success.data)
         )
 }
+
+// ----------------------FIREBASE NON CANCELLARE
+// export const getSlotList = (callback) => {
+//     const slotPath = firebaseDatabase.ref(DATABASE_REFERENCE.SLOT)
+//     slotPath.orderByChild('time').once('value')
+//         .then(
+//             (snapshot) => {
+//                 callback(snapshot.val())
+//             }
+//         )
+// }
 
 export const getSlotList = (callback) => {
-    const slotPath = firebaseDatabase.ref(DATABASE_REFERENCE.SLOT)
-    slotPath.orderByChild('time').once('value')
+    axios.get(`${databaseRoot}/Slots.json`)
         .then(
-            (snapshot) => {
-                callback(snapshot.val())
-            }
+            success => callback(success.data)
         )
 }
+
+
+// ----------------------FIREBASE NON CANCELLARE
+
+// export const getSlotWithId = (id, callback) => {
+//     firebaseDatabase.ref(`${DATABASE_REFERENCE.SLOT}/${id}`).once('value')
+//         .then(
+//             (snapshot) => callback(snapshot.val())
+//         )
+// }
+
 
 export const getSlotWithId = (id, callback) => {
-    firebaseDatabase.ref(`${DATABASE_REFERENCE.SLOT}/${id}`).once('value')
+    axios.get(`${databaseRoot}/Slots/${id}.json`)
         .then(
-            (snapshot) => callback(snapshot.val())
+            success => callback(success.data)
         )
 }
+
+// ----------------------FIREBASE NON CANCELLARE
+
+// export const deleteSlotWithId = (id, callback) => {
+//     firebaseDatabase.ref(`${DATABASE_REFERENCE.SLOT}/${id}`).remove()
+//         .then(
+//             () => {
+//                 console.log('deleteSlotWithId', id, callback)
+//                 callback()
+//             }
+//         ).catch(
+//             (error) => {
+//                 console.log('deleteSlotWithId', id, callback, error)
+//                 callback(error)
+//             }
+//         )
+//     // callback()
+// }
+
 
 export const deleteSlotWithId = (id, callback) => {
-    firebaseDatabase.ref(`${DATABASE_REFERENCE.SLOT}/${id}`).remove()
+    axios.delete(`${databaseRoot}/Slots/${id}.json`)
         .then(
-            () => {
-                console.log('deleteSlotWithId', id, callback)
-                callback()
-            }
-        ).catch(
-            (error) => {
-                console.log('deleteSlotWithId', id, callback, error)
-                callback(error)
-            }
+            () => callback()
         )
-    // callback()
-}
 
+}
 
 
