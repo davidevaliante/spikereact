9fc4b7610650aa131a67dd6f3571a84d5884df5e
Emanuele Grande <ema.grandem@gmail.com>
2018-09-24 18:34:52 +0200

    sistemati pulsanto

--- src/components/SlotPageComponents/ShortHandMenu.js
+++ src/components/SlotPageComponents/ShortHandMenu.js
@@ -3,9 +3,11 @@ import React from 'react'
 import { Button } from 'semantic-ui-react-single/Button'
 import { Icon } from 'semantic-ui-react-single/Icon'
 import { List } from 'semantic-ui-react-single/List'
+import { Responsive } from 'semantic-ui-react-single/Responsive'
 // router e redux
 import { connect } from 'react-redux'
 import { setUserPlaying } from '../../reducers/PlayModeReducer'
+import { RESPONSIVE_RESOLUTION } from '../../enums/Constants'
 
 const ShortHandMenu = (props) => {
 
@@ -18,43 +20,71 @@ const ShortHandMenu = (props) => {
 
     const buttonsStyle = {
         width: '165px', 
-        height: '40px'
+        height: '40px',
+        margin: '0 1.6rem'
     }
 
     return (
         <div className='shorthand-container-style'>
-            <div className='small-buttons-container'>
-                <List style={{ margin: '0 auto'}}>
-                    <List.Item>
-                        <Button
-                            style={buttonsStyle}
-                            inverted
-                            color='red'
-                            onClick={() => smoothScrollTo('slot-page-description')}>
-                            Descrizione
-                        </Button>
-                    </List.Item>
+            <Responsive maxWidth={RESPONSIVE_RESOLUTION.MEDIUM}>
+                <div className='small-buttons-container'>
+                    <List style={{ margin: '0 auto'}}>
+                        <List.Item>
+                            <Button
+                                style={buttonsStyle}
+                                inverted
+                                color='red'
+                                onClick={() => smoothScrollTo('slot-page-description')}>
+                                Descrizione
+                            </Button>
+                        </List.Item>
 
-                    <List.Item>
-                        <Button
-                            style={buttonsStyle}
-                            inverted
-                            color='red'
-                            onClick={() => smoothScrollTo('slot-page-lists')}>
-                            Consigli di gioco
-                        </Button>
-                    </List.Item>
-                    <List.Item>
-                        <Button
-                            style={buttonsStyle}
-                            inverted
-                            color={'red'}
-                            onClick={() => smoothScrollTo('slot-page-lists')}>
-                            Scheda tecnica
-                        </Button>
-                    </List.Item>
-                </List>
-            </div>
+                        <List.Item>
+                            <Button
+                                style={buttonsStyle}
+                                inverted
+                                color='red'
+                                onClick={() => smoothScrollTo('slot-page-lists')}>
+                                Consigli di gioco
+                            </Button>
+                        </List.Item>
+                        <List.Item>
+                            <Button
+                                style={buttonsStyle}
+                                inverted
+                                color={'red'}
+                                onClick={() => smoothScrollTo('slot-page-lists')}>
+                                Scheda tecnica
+                            </Button>
+                        </List.Item>
+                    </List>
+                </div>
+            </Responsive>
+            <Responsive minWidth={RESPONSIVE_RESOLUTION.MEDIUM}>
+                <div className='small-buttons-container'>
+                    <Button
+                        style={buttonsStyle}
+                        inverted
+                        color='red'
+                        onClick={() => smoothScrollTo('slot-page-description')}>
+                        Descrizione
+                    </Button>
+                 <Button
+                        style={buttonsStyle}
+                        inverted
+                        color='red'
+                        onClick={() => smoothScrollTo('slot-page-lists')}>
+                        Consigli di gioco
+                    </Button>
+                    <Button
+                        style={buttonsStyle}
+                        inverted
+                        color={'red'}
+                        onClick={() => smoothScrollTo('slot-page-lists')}>
+                        Scheda tecnica
+                    </Button>
+                </div>
+            </Responsive>
 
             <div className='big-buttons-container'>
                 <Button style={{ width: '45%' }} 
